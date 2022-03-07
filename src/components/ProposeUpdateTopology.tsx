5cc3d96db843ccce23fc1c425ac0b15ea8ac9484
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-03-06 23:50:48 -0500

    docs: + images, update topology interface

--- src/components/ProposeUpdateTopology.tsx
+++ src/components/ProposeUpdateTopology.tsx
@@ -54,8 +54,8 @@ const ProposeUpdateTopology: FC<IProposeProps> = ({ signer, mTx }) => {
 
 	const valueSelect = () => {
 		if (nodeLabels.length !== 0) {
-			const options = [0, 1, 2, 3].map((v) => {
-				return (<option key={`value-${v}`} value={v}>{v}</option>)
+			const options = ['none', 'primary industry', 'secondary industry', 'terterm industry'].map((v, i) => {
+				return (<option key={`value-${v}`} value={i}>{v}</option>)
 			})
 			return (
 				<select className="p-3 px-5 rounded-full bg-white font-semibold text-xl" name="value" onChange={valueChange}>
@@ -110,20 +110,26 @@ const ProposeUpdateTopology: FC<IProposeProps> = ({ signer, mTx }) => {
 	}
 
 	return (<>
-		<div className="flex flex-col max-w-lg mb-10 space-y-5">
+		<div className="flex flex-col mb-10 space-y-5">
 			<div>
 				<div className="font-semibold">Update Node Value:</div>
-				<p>changes the value of the selected node</p>
+				<p className="max-w-lg">changes the value of the selected node. The cooresponding node and its connections are shown below. The values indicate the following.</p>
+				<img className="w-full" src={`${process.env.PUBLIC_URL}topology.png`} alt="topology" />
 			</div>
-			<div className="flex flex-row justify-between pr-3 text-xl">
+			<div className="max-w-lg">
+			<div className="flex flex-row justify-between pr-3 text-xl mb-5 items-center">
 				<div>
-					label: {labelSelect()}
+					change zone {labelSelect()}
+				</div>
+				<div className="align-middle">
+				to
 				</div>
 				<div>
-					value: {valueSelect()}
+					{valueSelect()}
 				</div>
 			</div>
 			<button onClick={handleSubmit} disabled={disabled} className="p-3 bg-white border border-4 border-gray-800 font-semibold hover:bg-gray-500 hover:text-gray-100 hover:border-gray-500 active:bg-gray-800 active:border-gray-800 active:text-gray-100 disabled:border-gray-200 disabled:bg-gray-200 disabled:text-gray-400 rounded-full"> submit </button>
+		</div>
 		</div>
 	</>
 	)
